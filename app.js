e0794d6d2889a7eb39d3ae3ef13a4d8753537a81
tmy628 <tmy6h1r@gmail.com>
2021-04-09 12:34:24 +0000

    [Add] 文字列を結合する

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
-var hello = 'Hello World';
-// var 変数名 = 内容;
-alert(hello);
+// 変数を利用する場合には、それぞれの文字列を変数に格納して結合させる
+// var 変数名 = 内容 ;
+
+var str1 = 'Hello';
+var str2 = 'World!!';
+alert(str1 + str2);
+
+// 文字列と文字列を結合するには、+記号を使う
+// alert('Hello' + 'World');
+// 「Hello」と「World」の文字列が結合されて、 HelloWorld と表示される
